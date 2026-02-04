38047fc365ee677a96b17e2bc482457e0c1aa5d5
lizhenbang56 <38175894+lizhenbang56@users.noreply.github.com>
2026-02-04 16:27:36 +0800

    处理登录和提交订单的情况

--- GUI_Agent效果录屏/实验86：AI化/12306.py
+++ GUI_Agent效果录屏/实验86：AI化/12306.py
@@ -47,5 +47,25 @@ for train in trains:
     if 硬卧余票 != '--':
         print('有票')
         预订按钮 = train.ele('预订').click()
-        break
+        
+        # 弹出登录界面
+        try:
+            登录框 = tab.ele('.input')
+            登录框.input('13260295020')
+
+            密码框 = tab.ele('.login-item')
+            input('完成登录后，按任意键')
+        except Exception as e:
+            print('已经登录')
+            pass
+
+        tab = Chromium().latest_tab
+        提交订单按钮 = tab.ele('提交订单')
+        确认提交 = input('确认提交订单吗？y/n')
+        if 确认提交 == 'y':
+            提交订单按钮.click()
+        else:
+            print(提交订单按钮)
+            print('暂不提交')
+            exit(0)
     
